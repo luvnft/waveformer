716ff1b2c0a3a014071424862728be40ae13f9c5
The Wizard of Hahz <info@luvnft.com>
2024-05-14 08:35:42 -0400

    updated

--- components/FooterPromo.js
+++ components/FooterPromo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { ArrowRightIcon, Cog8ToothIcon, MusicalNoteIcon } from "@heroicons/react/24/solid";
+import { ArrowRightIcon, TelevisionIcon, MusicalNoteIcon } from "@heroicons/react/24/solid";
 
 const FooterPromo = () => {
   return (
@@ -8,22 +8,22 @@ const FooterPromo = () => {
       <div className="p-6 mb-6 bg-gray-100 rounded-lg">
         <div className="max-w-prose">
           <h2 className="font-bold md:text-2xl text-xl mb-4">
-            <Cog8ToothIcon className="h-6 w-6 inline-block pb-1"></Cog8ToothIcon> Replicate
+            <TelevisionIcon className="h-6 w-6 inline-block pb-1"></TelevisionIcon> NFTV Discord
           </h2>
-          <p className="mb-4">Replicate lets you run machine learning models with a few lines of code, without needing to understand how machine learning works.</p>
-          <Link href="https://replicate.com/docs?utm_source=project&utm_campaign=waveformer" className="bg-black text-white px-5 py-3 mt-2 rounded" type="submit">
-            Build with Replicate <ArrowRightIcon className="h-5 w-5 inline-block"></ArrowRightIcon>
+          <p className="mb-4"> Sell your AImusic in our NFTV Discord as a broker in the marketplace, your first AImusic LUV NFT is on the house for a limited time only.</p>
+          <Link href="https://nftv.luvnft.com" className="bg-black text-white px-5 py-3 mt-2 rounded" type="submit">
+            Subscribe to NFTV <ArrowRightIcon className="h-5 w-5 inline-block"></ArrowRightIcon>
           </Link>
         </div>
       </div>
       <div className="p-6 mb-6 bg-gray-100 rounded-lg">
         <div className="max-w-prose">
           <h2 className="font-bold md:text-2xl text-xl mb-4">
-            <MusicalNoteIcon className="h-6 w-6 inline-block"></MusicalNoteIcon> MusicGen
+            <MusicalNoteIcon className="h-6 w-6 inline-block"></MusicalNoteIcon> AImusic
           </h2>
-          <p className="mb-4">MusicGen is a <Link className="underline" href="https://github.com/facebookresearch/audiocraft">model developed by Facebook Research</Link> (Jade Copet, Felix Kreuk, et al).</p>
+          <p className="mb-4">AImusic was made with LUV by <Link className="underline" href="https://linkedin.com/in/hahzterry">The Wizard of Hahz</Link></p>
           <p className="mb-4">It uses AI to create music. It was trained on 20,000 hours of licensed music.</p>
-          <p><Link className="underline" href="https://github.com/facebookresearch/audiocraft/blob/main/MODEL_CARD.md">Model card</Link></p>
+          <p><Link className="underline" href="mailto:info@luvnft.com">Contact for special AImusic requests</Link></p>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -18,16 +18,16 @@ export default function Home({ prompt }) {
     <div>
       <div className="container max-w-4xl mx-auto md:px-8 px-4 py-4 pb-10 bg-white border-black min-h-screen">
         <Head>
-          <title>Waveformer</title>
+          <title>AIMUSIC</title>
           <Meta />
         </Head>
 
         <h1 className="calistoga md:text-8xl text-5xl text-black text-center mb-6 pt-10">
-          Waveformer
+          AIMUSIC
         </h1>
 
         <p className="text-center mb-10 -mt-2 md:text-2xl text-xl">
-          Make music from text with <Link href="https://replicate.com/meta/musicgen?utm_source=project&utm_campaign=waveformer" className="hover:bg-violet-900 underline md:underline-offset-4 hover:no-underline bg-black text-white md:px-2 px-1 md:py-1 rounded">MusicGen and Replicate</Link>
+          Make music from text then tokenize it as a <Link href="https://luvnft.com" className="hover:bg-violet-900 underline md:underline-offset-4 hover:no-underline bg-black text-white md:px-2 px-1 md:py-1 rounded">LUV NFT</Link> .
         </p>
 
         <Card>
